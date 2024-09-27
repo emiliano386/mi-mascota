af0b5ccc1a447a4d6040d800b84c829c977923d7
emiliano386 <emilianosantana386@gmail.com>
2024-09-26 23:47:18 -0300

    Actualizar archivo de configuración para evitar conflictos con el backend en Vercel

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -24,7 +24,7 @@ const Registro = () => {
     setSuccessMessage(''); // Limpiar el mensaje de éxito al intentar registrar
 
     try {
-      const response = await fetch('http://localhost:5000/api/registrar', {
+      const response = await fetch('https://mi-mascota-hazel.vercel.app/api/registro', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
